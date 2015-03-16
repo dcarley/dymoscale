02a23ed9a7f32d1691b7e9fa26e122e7a41356f9
Dan Carley <dan.carley@gmail.com>
2015-03-16 08:04:26 +0000

    Group tests by object weight
    So that we don't have to repeat the expected value and it's clear that the
    readings are of the same object. This is best viewed with `git show -b` to
    discard whitespace.

--- dymoscale_test.go
+++ dymoscale_test.go
@@ -64,194 +64,214 @@ var _ = Describe("Dymoscale", func() {
 			Expect(grams).To(Equal(0))
 		})
 
-		It("should read 136g object", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 136, 0, 0, 0})
+		Describe("136g (4.8oz) object", func() {
+			expectedGrams := 136
 
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   Stable,
-				Mode:        Grams,
-				ScaleFactor: 0,
-				WeightMinor: 136,
-				WeightMajor: 0,
-			}))
+			It("should read from grams mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 136, 0, 0, 0})
 
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(Equal(136))
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   Stable,
+					Mode:        Grams,
+					ScaleFactor: 0,
+					WeightMinor: 136,
+					WeightMajor: 0,
+				}))
+
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(Equal(expectedGrams))
+			})
+
+			It("should read from ounces mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 48, 0, 0, 0})
+
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   Stable,
+					Mode:        Ounces,
+					ScaleFactor: -1,
+					WeightMinor: 48,
+					WeightMajor: 0,
+				}))
+
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(BeNumerically("~", expectedGrams, OuncesToGramsVariation))
+			})
 		})
 
-		It("should read 4.8oz object (136g)", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 48, 0, 0, 0})
+		Describe("418g (14.7oz) object", func() {
+			expectedGrams := 418
 
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   Stable,
-				Mode:        Ounces,
-				ScaleFactor: -1,
-				WeightMinor: 48,
-				WeightMajor: 0,
-			}))
+			It("should read from grams mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 162, 1, 0, 0})
 
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(BeNumerically("~", 136, OuncesToGramsVariation))
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   Stable,
+					Mode:        Grams,
+					ScaleFactor: 0,
+					WeightMinor: 162,
+					WeightMajor: 1,
+				}))
+
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(Equal(expectedGrams))
+			})
+
+			It("should read from ounces mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 147, 0, 0, 0})
+
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   Stable,
+					Mode:        Ounces,
+					ScaleFactor: -1,
+					WeightMinor: 147,
+					WeightMajor: 0,
+				}))
+
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(BeNumerically("~", expectedGrams, OuncesToGramsVariation))
+			})
 		})
 
-		It("should read 418g object", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 162, 1, 0, 0})
+		Describe("1384g (3lb 0.8oz) object", func() {
+			expectedGrams := 1384
 
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   Stable,
-				Mode:        Grams,
-				ScaleFactor: 0,
-				WeightMinor: 162,
-				WeightMajor: 1,
-			}))
+			It("should read from grams mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 104, 5, 0, 0})
 
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(Equal(418))
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   Stable,
+					Mode:        Grams,
+					ScaleFactor: 0,
+					WeightMinor: 104,
+					WeightMajor: 5,
+				}))
+
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(Equal(expectedGrams))
+			})
+
+			It("should read from ounces mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 232, 1, 0, 0})
+
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   Stable,
+					Mode:        Ounces,
+					ScaleFactor: -1,
+					WeightMinor: 232,
+					WeightMajor: 1,
+				}))
+
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(BeNumerically("~", expectedGrams, OuncesToGramsVariation))
+			})
 		})
 
-		It("should read 14.7oz object (418g)", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 147, 0, 0, 0})
+		Describe("2492g (5lb 7.9oz) object", func() {
+			expectedGrams := 2492
 
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   Stable,
-				Mode:        Ounces,
-				ScaleFactor: -1,
-				WeightMinor: 147,
-				WeightMajor: 0,
-			}))
+			It("should read from grams mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 188, 9, 0, 0})
 
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(BeNumerically("~", 418, OuncesToGramsVariation))
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   Stable,
+					Mode:        Grams,
+					ScaleFactor: 0,
+					WeightMinor: 188,
+					WeightMajor: 9,
+				}))
+
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(Equal(expectedGrams))
+			})
+
+			It("should read from ounces mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 111, 3, 0, 0})
+
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   Stable,
+					Mode:        Ounces,
+					ScaleFactor: -1,
+					WeightMinor: 111,
+					WeightMajor: 3,
+				}))
+
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(BeNumerically("~", expectedGrams, OuncesToGramsVariation))
+			})
 		})
 
-		It("should read 1384g object", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 104, 5, 0, 0})
+		Describe("4294g (9lb 7.5oz) object", func() {
+			expectedGrams := 4294
 
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   Stable,
-				Mode:        Grams,
-				ScaleFactor: 0,
-				WeightMinor: 104,
-				WeightMajor: 5,
-			}))
+			It("should read from grams mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 198, 16, 0, 0})
 
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(Equal(1384))
-		})
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   4,
+					Mode:        2,
+					ScaleFactor: 0,
+					WeightMinor: 198,
+					WeightMajor: 16,
+				}))
 
-		It("should read 3lb 0.8oz object (1384g)", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 232, 1, 0, 0})
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(Equal(expectedGrams))
+			})
 
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   Stable,
-				Mode:        Ounces,
-				ScaleFactor: -1,
-				WeightMinor: 232,
-				WeightMajor: 1,
-			}))
+			It("should read from ounces mode", func() {
+				buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 234, 5, 0, 0})
 
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(BeNumerically("~", 1384, OuncesToGramsVariation))
-		})
+				reading, err := ReadMeasurement(buf)
+				Expect(err).To(BeNil())
+				Expect(reading).To(Equal(&Measurement{
+					AlwaysThree: 3,
+					Stability:   4,
+					Mode:        11,
+					ScaleFactor: -1,
+					WeightMinor: 234,
+					WeightMajor: 5,
+				}))
 
-		It("should read 2492g object", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 188, 9, 0, 0})
-
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   Stable,
-				Mode:        Grams,
-				ScaleFactor: 0,
-				WeightMinor: 188,
-				WeightMajor: 9,
-			}))
-
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(Equal(2492))
-		})
-
-		It("should read 5lb 7.9oz object (2492g)", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 111, 3, 0, 0})
-
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   Stable,
-				Mode:        Ounces,
-				ScaleFactor: -1,
-				WeightMinor: 111,
-				WeightMajor: 3,
-			}))
-
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(BeNumerically("~", 2492, OuncesToGramsVariation))
-		})
-
-		It("should read 4294g object", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 2, 0, 198, 16, 0, 0})
-
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   4,
-				Mode:        2,
-				ScaleFactor: 0,
-				WeightMinor: 198,
-				WeightMajor: 16,
-			}))
-
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(Equal(4294))
-		})
-
-		It("should read 9lb 7.5oz object (4294g)", func() {
-			buf := bytes.NewBuffer([]byte{3, 4, 11, 255, 234, 5, 0, 0})
-
-			reading, err := ReadMeasurement(buf)
-			Expect(err).To(BeNil())
-			Expect(reading).To(Equal(&Measurement{
-				AlwaysThree: 3,
-				Stability:   4,
-				Mode:        11,
-				ScaleFactor: -1,
-				WeightMinor: 234,
-				WeightMajor: 5,
-			}))
-
-			grams, err := reading.Grams()
-			Expect(err).To(BeNil())
-			Expect(grams).To(BeNumerically("~", 4294, OuncesToGramsVariation))
+				grams, err := reading.Grams()
+				Expect(err).To(BeNil())
+				Expect(grams).To(BeNumerically("~", expectedGrams, OuncesToGramsVariation))
+			})
 		})
 	})
 })
